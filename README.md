32c4a99be08c79614c51cd4ec487123f1fada971
asvdvl <57688070+asvdvl@users.noreply.github.com>
2024-11-10 21:40:13 +0300

    release for 1.1

--- README.md
+++ README.md
@@ -1,19 +1,5 @@
-# factorio-mod-template - template for my mods
+replaces combinator sounds with dial-up sounds
 
-how to apply:
-1. rename folder with new mod name
-1. open `info.json`
-1. change:
-    - title
-    - name
-    - description
-    - homepage
-1. change `LICENSE`
-1. shtamp current date in `changelog.txt`
-1. commit changes with message `first mod commit after template`
-- make the mod(develop it)
+Original files recorded by me with 2 Acorp 56k and one from [wikipedia](https://en.wikipedia.org/wiki/File:Dial_up_modem_noises.ogg)
 
-### before publish
-1. delete all emply(unused) files
-1. edit README files
-1. describe what was done in `changelog.txt` with version and date increment
+[Busy signal](https://en.wikipedia.org/wiki/File:NorthAmericanBusySignal.ogg)
---
+++ data-updates.lua
@@ -0,0 +1,35 @@
+local combinator_proto_cats = {
+    "arithmetic-combinator",
+    "constant-combinator",
+    "decider-combinator",
+    "selector-combinator"
+}
+local sound_definition_file_names = {
+    "01_wiki.ogg",
+    "02_9600_dial-up.ogg",
+    "03_115200_dial-up.ogg",
+    "04_300_not-conneected_dial-up.ogg",
+}
+local path_prefix = "__dial-up-combinators__/sound/"
+
+local sound_definition = {
+    variations = {}
+}
+for _, name in pairs(sound_definition_file_names) do
+    table.insert(sound_definition.variations, {
+        filename = path_prefix..name,
+        volume = 1
+    })
+end
+
+for _, name in pairs(combinator_proto_cats) do
+    if data.raw[name] then
+        for _, proto in pairs(data.raw[name]) do
+            if proto.working_sound then
+                proto.working_sound.activate_sound = sound_definition
+                proto.working_sound.sound = {filename = path_prefix.."/noise.ogg"}
+                proto.deactivate_sound = {filename = path_prefix.."BusySignal.ogg", volume = 0.5}
+            end
+        end
+    end
+end
